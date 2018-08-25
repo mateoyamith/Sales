e879f4de4f635ebf1a7bbc31e44d91d94a1a2793
mateoyamith <mateosanchez9602@gmail.com>
2018-08-24 21:29:38 -0500

    parte 15 y 16

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -29,6 +29,20 @@
         [DataType(DataType.Date)]
         public DateTime PublishOn { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(this.ImagePath))
+                {
+                    return null;
+                }
+
+                return $"https://salesbackend.azurewebsites.net/{this.ImagePath.Substring(1)}";
+            }
+
+        }
+
         public override string ToString()
         {
             return this.Description;                 
