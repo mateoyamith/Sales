df039d20e1bbc6500e24e5e1451d90a66d34dffb
mateoyamith <mateosanchez9602@gmail.com>
2018-08-30 21:59:39 -0500

    Parte 22

--- Sales/Sales/Helpers/Languages.cs
+++ Sales/Sales/Helpers/Languages.cs
@@ -76,5 +76,15 @@
         {
             get { return Resource.ChangeImage; }
         }
+
+        public static string DescriptionError
+        {
+            get { return Resource.DescriptionError; }
+        }
+
+        public static string PriceError
+        {
+            get { return Resource.PriceError; }
+        }
     }
 }
